6eccc609a7add69d06b3e40ebba1b48bafd32bb1
VictorKenichi <you@example.com>
2020-01-29 23:17:57 +0100

    Question 19 added

--- src/GeneTraj.m
+++ src/GeneTraj.m
@@ -1,6 +1,12 @@
 function qc = GeneTraj(qdi, qdf, t)
 
 tf = 0.5;                         % Temps final de suivi de la trajectoire
-qc = qdi + (6*(t/tf)^5 - 15*(t/tf)^4 + 10*(t/tf)^3) * (qdf - qdi);
 
+if t < tf 
+    qc = qdi + (6*(t/tf)^5 - 15*(t/tf)^4 + 10*(t/tf)^3) * (qdf - qdi);
+else
+    qc = qdf;
+end
+
+%qc = qdi;
 end
---
+++ src/Q19Test.m
@@ -0,0 +1,10 @@
+%% Question 19
+clear;
+clc;
+
+
+%% Teste le modèle simulink
+q_di = [-1.00; 0.00; -1.00; -1.00; -1.00; -1.00];   % coordonnées articulaires initiales
+q_df = [ 0.00; 1.00;  0.00;  0.00;  0.00;  0.00];
+
+sim('Generation_de_trajectoire', 'ReturnWorkspaceOutputs', 'on');
